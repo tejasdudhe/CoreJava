2f569e213ed2b43bb69e73579d8df9c6d66497e7
“tejasdudhe” <tejasdudhe12@gmail.com>
2024-10-02 23:07:13 +0530

    day8

--- CoreJavaBook1/src/day3/Fibbonachi.java
+++ CoreJavaBook1/src/day3/Fibbonachi.java
@@ -8,6 +8,20 @@ public class Fibbonachi {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter the no of of Fibb Number to show : ");
+		int f = sc.nextInt();
+		
+		int num1 = 0, num2 =1;
+		int num3=0;
+		
+		for(int i=0 ; i<f;i++)
+		{
+			System.out.print(num1+" ");
+			num3 = num1 +num2;
+			num1 = num2;
+			num2 = num3; 
+			
+		}
 		
 	}
 	
